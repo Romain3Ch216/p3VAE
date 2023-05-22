54742d067cc9280ef7d3ba0c365d0e79aa1fe05d
Romain Thoreau <romain.thoreau@outlook.fr>
2023-05-22 09:29:52 +0200

    Update and rename stat_test.R to stat_test.py

--- stat_test.py
+++ stat_test.py
@@ -1,3 +1,5 @@
+from scipy.stats import shapiro, levene, ttest_ind
+
 score_q_no_gs = c(0.8771746001865054,
             0.9034996070969992,
             0.8962500634004125,
@@ -20,10 +22,14 @@ score_q_gs = c(0.9231048819614895,
             0.8831735231554051,
             0.9031859306329602)
 
-shapiro.test(score_q_no_gs)
+shapiro_test = shapiro(score_q_no_gs)
+print(shapiro_test.pvalue)
 
-shapiro.test(score_q_gs)
+shapiro_test = shapiro(score_q_gs)
+print(shapiro_test.pvalue)
 
-var.test(score_q_no_gs, score_q_gs)
+var_test = levene(score_q_no_gs, score_q_gs)
+print(var_test.pvalue)
 
-t.test(score_q_no_gs, score_q_gs, var.equal = TRUE, alternative = "less")
+student_test = ttest_ind(score_q_no_gs, score_q_gs, equal_var=True, alternative='less')
+print(student_test.pvalue)
